100f09ba91ab31a5d480d843f24733e492dccf09
CK <chun_kiat88@hotmail.com>
2020-12-07 20:02:09 +0800

    2003

---
+++ week05/scripts/geolocation.js
@@ -0,0 +1,14 @@
+let ellocate = document.getElementById("locate");
+ellocate.addEventListener("click",function(){
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        let ellat = document.getElementById("lat");
+        let ellong = document.getElementById("long");
+
+        ellat.innerHTML = "Latitude: " + position.coords.latitude;
+        ellong.innerHTML = "Longtitude: " + position.coords.longitude;
+    });
+} else {
+    alert("Geolocation is not supported by this browser.")
+}
+});
