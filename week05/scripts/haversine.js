4298c7eec53a479cf16bebdff753174e6ae7eb2e
CK <chun_kiat88@hotmail.com>
2020-12-07 21:39:56 +0800

    2140

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -26,9 +26,9 @@ function roundVal(val){
 }
 
 function calculateDistances(lat, lon) {
-    let d1 = harversine(lat, lon, 2.922562, 101.650965); //de pulze, cyberjaya
-    let d2 = harversine(lat, lon, 3.073065, 101.607787); //Sunway Pyramid
-    let d3 = harversine(lat, lon, 3.158761, 101.714524); //KLCC
+    let d1 = haversine(lat, lon, 2.922562, 101.650965); //de pulze, cyberjaya
+    let d2 = haversine(lat, lon, 3.073065, 101.607787); //Sunway Pyramid
+    let d3 = haversine(lat, lon, 3.158761, 101.714524); //KLCC
 
     return [d1, d2, d3];
 }
