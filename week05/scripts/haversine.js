972fc09c8e65cbf3858a3b6372ef3471bd26451f
CK <chun_kiat88@hotmail.com>
2020-12-07 21:08:11 +0800

    2109

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -2,14 +2,15 @@ function haversine(lat1, lon1, lat2, lon 2){
     let R = 6171; //earth radius in KM
     let dLat = (lat2-lat1).toRad();
     let dLon = (lon2-lon1).toRad();
-    let lat1 = lat1.toRad();
-    let lat1 = lat2.toRad();
+    lat1 = lat1.toRad();
+    lat1 = lat2.toRad();
 
     let a = Math.sin(dLat/2) * Math.sin(dLat/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
     let c = 2* Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
     let d = R * c;
 
     return roundVal(d);
+    
 }
 
 if (typeof Number.prototype.toRad == "undefined") {
