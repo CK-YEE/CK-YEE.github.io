217a98c2f28fcfc5b1bfca53a4c8f728ef12c223
CK <chun_kiat88@hotmail.com>
2020-12-22 21:16:29 +0800

    2117

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -22,7 +22,8 @@ function GetBookings() {
                 row.insertCell(2).innerHTML = gEmail;
                 row.insertCell(3).innerHTML = gPax;
                 row.insertCell(4).innerHTML = gRemarks;
-                row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button>";
+                row.insertCell(5).innerHTML = "<button id='" +
+                    buttonId + "' class='btn btn-danger'>Delete</button>";
 
                 bookingIds.push(buttonId);
 
